85e23e3a243f5cb590c041a831296d9d373eb2ef
taomengxia1 <taomengxia@nctieda.com>
2025-10-14 11:34:16 +0800

    [Miter][US-011]：Support Miter to AIGER/BTOR2【add variadic-ops lower before HWToBTOR2】

---
+++ test/integration_tests/cases/leading_zero_dichotomy_run_bitwuzla_with_btor2/leading_zero_dichotomy_run_bitwuzla_with_btor2.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+set -euo pipefail
+
+CASE_DIR=$(dirname "$(realpath "$0")")
+OUTPUT_DIR=${CASE_LOG_PATH:-.}
+
+EXECUTE_SCRIPT="$CASE_DIR/../execute_solver_with_btor2.sh"
+
+#CHECK: unsat
+"$EXECUTE_SCRIPT" bitwuzla "lzc7_dichotomy" "lzc7_dichotomy" "$OUTPUT_DIR" "$CASE_DIR/LZC_DICHOTOMY.mlir"
+
