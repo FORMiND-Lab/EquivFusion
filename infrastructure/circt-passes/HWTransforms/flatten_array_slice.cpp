4bca00d5ed9b15a1e505cd4ff6f733b41f1caf6d
Mengxia Tao <taomengxia@nctieda.com>
2025-12-09 15:55:46 +0800

    [Miter][US-019]: in FlattenArraySlicePass special hw.array_slice treat as hw.array_get (#64)

--- infrastructure/circt-passes/HWTransforms/flatten_array_slice.cpp
+++ infrastructure/circt-passes/HWTransforms/flatten_array_slice.cpp
@@ -19,7 +19,29 @@ struct ArraySliceOpConversion : OpRewritePattern<hw::ArraySliceOp> {
     using OpRewritePattern<hw::ArraySliceOp>::OpRewritePattern;
 
     LogicalResult matchAndRewrite(hw::ArraySliceOp op, PatternRewriter &rewriter) const override {
+        auto inputArrayType = cast<hw::ArrayType>(op.getInput().getType());
         auto resArrayType = cast<hw::ArrayType>(op.getType());
+
+        /* Special hw.array_slice: need hw.array_get  [CIRCT version: firtool-1.132.0]
+         * Verilog:
+         *      module array(input in_2dim[1:0][1:0], input idx, output o);
+         *          assign o = in_2dim[idx][idx];
+         *      endmodule
+         * MLIR:
+         *      module {
+         *          hw.module @array(in %in_2dim : !hw.array<2xarray<2xi1>>, in %idx : i1, out o : i1) {
+         *              %0 = hw.array_slice %in_2dim[%idx] : (!hw.array<2xarray<2xi1>>) -> !hw.array<2xi1>
+         *              %1 = hw.array_get %0[%idx] : !hw.array<2xi1>, i1
+         *              hw.output %1 : i1
+         *          }
+         *      }
+        */
+        if (resArrayType == inputArrayType.getElementType()) {
+            rewriter.replaceOpWithNewOp<hw::ArrayGetOp>(op, op.getInput(), op.getLowIndex());
+            return success();
+        }
+
+        assert(inputArrayType.getElementType() == resArrayType.getElementType());
         size_t sliceSize = resArrayType.getNumElements();
         if (sliceSize == 0)
             return failure();
