de26870de3911831c12337bdb9b80d62dd930ac5
taomengxia1 <taomengxia@nctieda.com>
2025-12-26 17:25:35 +0800

    update

--- solving/solver-runner/solver_declare.cpp
+++ solving/solver-runner/solver_declare.cpp
@@ -1,10 +1,10 @@
-//===-----------------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the EquivFusion Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-//===-----------------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 
 #include "solving/solver-runner/solver_declare.h"
 
